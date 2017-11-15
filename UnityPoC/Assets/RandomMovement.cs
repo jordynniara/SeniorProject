8f66b3abe8b7a476af8591b1aa59d65b439cec93
Corithian Williams <corithianmw96@gmail.com>
2017-11-15 13:31:18 -0500

    Made RandomMovement implement move

--- UnityPoC/Assets/RandomMovement.cs
+++ UnityPoC/Assets/RandomMovement.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomMovement : Movement {
+    public void move()
+    {
 
-	// Use this for initialization
-	void Start () {
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
