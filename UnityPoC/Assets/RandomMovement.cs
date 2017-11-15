693b58dc4ed4075ca2abf8de6b9caccd4218345a
Corithian Williams <corithianmw96@gmail.com>
2017-11-15 13:25:01 -0500

    Changed skeletons

--- UnityPoC/Assets/RandomMovement.cs
+++ UnityPoC/Assets/RandomMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Entity : MonoBehaviour {
+public class RandomMovement : Movement {
 
 	// Use this for initialization
 	void Start () {
