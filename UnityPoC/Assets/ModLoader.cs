1c417b5a188add23a8bbd3998d6df321a7b799be
Corithian Williams <corithianmw96@gmail.com>
2017-11-19 22:09:38 -0500

    Reading test data in Unity

--- UnityPoC/Assets/ModLoader.cs
+++ UnityPoC/Assets/ModLoader.cs
@@ -1,12 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class ModLoader : MonoBehaviour {
 
+    [Serialiable]
+    public class EntityInfo
+    {
+
+        public List<Entity> contents;
+    }
+
+    public static EntityInfo data;
+
 	// Use this for initialization
 	void Start () {
-		
+        StreamReader r = new StreamReader("abcd.txt");
+        string contents = r.ReadToEnd();
+        data = JsonUtility.FromJson<EntityInfo>(contents);
+        for (int i = 0; i < data.contents.Count; i++)
+        {
+            Debug.Log(data.contents[i]);
+        }
 	}
 	
 	// Update is called once per frame
