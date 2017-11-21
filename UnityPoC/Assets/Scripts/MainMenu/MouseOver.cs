3e3478bf7cac4d6a7ead3e258a08daa5764122fd
jordynniara <jnsmith96@yahoo.com>
2017-11-20 21:40:18 -0500

    Start Menu
    Created a Start menu with buttons in Unity project

---
+++ UnityPoC/Assets/Scripts/MainMenu/MouseOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MouseOver : MonoBehaviour {
+	void Start(){
+		GetComponent<Renderer> ().material.color = Color.white;
+	}
+
+	void OnMouseEnter(){
+		GetComponent<Renderer> ().material.color = Color.red;
+	}
+
+	void OnMouseExit() {
+		GetComponent<Renderer> ().material.color = Color.white;
+	}
+}
