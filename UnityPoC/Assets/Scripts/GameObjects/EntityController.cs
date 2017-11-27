0888276bb8b500a401fa7d5989e07db94c2387e9
jordynniara <jnsmith96@yahoo.com>
2017-11-26 19:22:32 -0500

    added movement to character

--- UnityPoC/Assets/Scripts/GameObjects/EntityController.cs
+++ UnityPoC/Assets/Scripts/GameObjects/EntityController.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 //Basically just has a movement stuck to it
 public class EntityController : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-    }
+	void Update ()
+	{
+	}
 }
+
--- UnityPoC/Assets/Scripts/GameObjects/KeyboardMovement.cs
+++ UnityPoC/Assets/Scripts/GameObjects/KeyboardMovement.cs
@@ -4,11 +4,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //So basically in the move method, we need to check if whatever arrow key/wasd whatever you want is pressed, then move this object in that direction
-public class KeyboardMovement : MonoBehaviour, Movement {
-    public float speed;
+public class KeyboardMovement : MonoBehaviour, Movement{
+	public float speed = 3.5f;
     public void move()
     {
-        
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
     }
 
     // Use this for initialization
