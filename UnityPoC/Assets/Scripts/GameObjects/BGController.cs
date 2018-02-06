bb66d562bd4a364568b80c53097f82beb7cf7bb1
Corithian Williams <corithianmw96@gmail.com>
2018-02-06 13:29:00 -0500

    Added background scrolling

---
+++ UnityPoC/Assets/Scripts/GameObjects/BGController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGController : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(0, speed * Time.fixedDeltaTime);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        while (transform.position.y < -10.0f) transform.position += new Vector3(0, 10.0f);
+        while (transform.position.y > 10.0f) transform.position -= new Vector3(0, 10.0f);
+    }
+}
