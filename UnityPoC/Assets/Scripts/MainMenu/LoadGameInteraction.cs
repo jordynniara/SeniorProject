795366d30e2aaea6f1b053c621dbdd052bc5a93d
kbillie <kaila@kailas-mbp.ncat.edu>
2018-02-14 21:17:29 -0500

    -added “The Lab” as well as the ability to go to it from the main menu

---
+++ UnityPoC/Assets/Scripts/MainMenu/LoadGameInteraction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGameInteraction : MonoBehaviour {
+
+
+	void OnMouseUpAsButton () {
+		SceneManager.LoadScene("TheLab");	
+	}
+}
