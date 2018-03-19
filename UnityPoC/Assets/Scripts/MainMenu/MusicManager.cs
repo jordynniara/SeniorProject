095e9e64219057d1646ec45cca87e5ebdfea2db4
jordynniara <jnsmith96@yahoo.com>
2018-03-19 13:40:35 -0400

    Added sounds quit option in main menu

---
+++ UnityPoC/Assets/Scripts/MainMenu/MusicManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource audio;
+    static bool AudioBegin = false;
+    void Awake()
+    {
+        if (!AudioBegin)
+        {
+            audio.Play();
+            DontDestroyOnLoad(gameObject);
+            AudioBegin = true;
+        }
+    }
+    void Update()
+    {
+        if (SceneManager.GetActiveScene() != SceneManager.GetSceneByName("TheLab") ||
+            SceneManager.GetActiveScene() != SceneManager.GetSceneByName("MainMenu"))
+        {
+            audio.Stop();
+            AudioBegin = false;
+        }
+    }
+}
